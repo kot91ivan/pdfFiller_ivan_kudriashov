dff0cc01245de5cb372bd9f1accdc435450654c1
Ivan Kudriashov <kot91ivan@gmail.com>
2023-05-16 17:21:43 +0300

    add styles for button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,10 +2,22 @@ import React from "react"
 import classnames from "classnames"
 import s from "./Button.module.scss"
 
-const Button = ({ children, text, isPrimary, disabled, label }) => {
+const Button = ({
+  children,
+  text,
+  isPrimary,
+  isWide,
+  isMobile,
+  disabled,
+  label
+}) => {
   return (
     <button
-      className={classnames(s.button, { [s.primary]: isPrimary })}
+      className={classnames(s.button, {
+        [s.primary]: isPrimary,
+        [s.is_wide]: isWide,
+        [s.is_mobile]: isMobile
+      })}
       disabled={disabled}
       aria-label={label}
     >
